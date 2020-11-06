287bbb1eccb6cf1923f486929974e51755b9e308
Rajkumar Rajangam <45938200+RajkumarRajangam@users.noreply.github.com>
2020-11-06 17:24:19 +0100

    Update createws.tf

--- createws.tf
+++ createws.tf
@@ -1,16 +1,1 @@
-provider "tfe" {
-  hostname = var.hostname
-  token = var.tfe_token
-}
 
-module "tfe-workspace" {
-  source  = "app.terraform.io/tenant-raj/tfe-workspace/tfe"
-  version = "1.0.1"
-  organization_name = var.organization_name
-  workspace_name = var.workspace_name
-}
-
-  variable "organization_name" {}
-  variable "workspace_name" {}
-  variable "hostname" {}
-  variable "tfe_token" {}
